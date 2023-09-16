88dc94c05c42d190259467de6c6f8141396c6916
Roshan <shahroshan0720@gmail.com>
2023-09-16 17:18:20 +0530

    api project added

--- data/apiprojects.js
+++ data/apiprojects.js
@@ -28,12 +28,69 @@ const apiProjects =[
         color: '#13fc97'
     },
     {
-        title: "",
-        imgPath: '/images/api/.jpg',
-        gitLink: '',
-        url: '',
-        color: ''
+        title: "Image Search",
+        imgPath: '/images/api/imagesearch.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-X-API/tree/main/Image%20Search%20App',
+        url: 'https://image-search-adda07.netlify.app/',
+        color: 'brown'
     },
+    {
+        title: "Quiz",
+        imgPath: '/images/api/quiz.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-X-API/tree/main/Quiz%20App',
+        url: 'https://mcqquiz-app07.netlify.app/',
+        color: '#19d6d675'
+    },
+    {
+        title: "Recipe Viewer",
+        imgPath: '/images/api/reciepe.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-X-API/tree/main/Recipe%20App',
+        url: 'https://kitchenrecipe07.netlify.app/',
+        color: '#d2945e'
+    },
+    {
+        title: "QR Code Generator",
+        imgPath: '/images/api/qrgen.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-X-API/tree/main/QR%20Code%20Generator',
+        url: 'https://qr-code-gen07.netlify.app/',
+        color: '#7fb7c9'
+    },
+    {
+        title: "Email Subscription",
+        imgPath: '/images/api/subcription.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-X-API/tree/main/Email%20Subscription%20Form',
+        url: 'https://email-subscription07.netlify.app/',
+        color: '#61b752'
+    },
+    {
+        title: "MCU Comic Character Viewer",
+        imgPath: '/images/api/mcu.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-X-API/tree/main/MCU%20APP',
+        url: 'https://mcu-characters07.netlify.app/',
+        color: 'black'
+    },
+    {
+        title: "Pokemon Card Generator",
+        imgPath: '/images/api/pokemon.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-X-API/tree/main/Pokemon%20Card%20Generator',
+        url: 'https://pokemon-card07.netlify.app/',
+        color: '#fed330'
+    },
+    {
+        title: "Weather Report",
+        imgPath: '/images/api/weather.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-X-API/tree/main/Weather%20App',
+        url: 'https://mausamdekho-app07.netlify.app/',
+        color: '#62b8f5'
+    },
+    {
+        title: "Speed Typing Game",
+        imgPath: '/images/api/typing.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-X-API/tree/main/Speed%20Typing%20Game',
+        url: '#',
+        color: '#F0DB4F'
+    },
+
 ];
 
 export default apiProjects;
