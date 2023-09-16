9d061713bdc5c5fb5e40d6d11dc877a9b4d59b69
Roshan <shahroshan0720@gmail.com>
2023-09-16 18:07:17 +0530

    disable link

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import apiProjects from './data/apiprojects.js';
 
-const projectDOM = document.querySelector('.projects-center.fsa');
+const jxaDOM = document.querySelector('.projects-center.jxa');
 
-const displayProducts = (data)=>{
-    const projectList = data.map((project)=>{
+const displayProjects = (data) => {
+    const projectList = data.map((project) => {
         const title = project.title;
         const imgPath = project.imgPath;
         const gitLink = project.gitLink;
@@ -11,24 +11,35 @@ const displayProducts = (data)=>{
         const color = project.color;
 
         return `
-        <article class="project">
-        <div class="img-container" style="background-color : ${color};">
-            <img src=${imgPath} alt="" class="project-img img">
-        </div>
-        <footer class="project-footer">
-            <h5 class="project-name">${title}</h5>
-            <div class="icons">
-                <a class="project-link" href="${gitLink}" target="_blank"><i class="fa-brands fa-github"></i></a>
-                <a class="project-link" href="${url}" target="_blank"><i class="fa-solid fa-link"></i></a>
-            </div>
+            <article class="project">
+                <div class="img-container" style="background-color : ${color};">
+                    <img src=${imgPath} alt="" class="project-img img">
+                </div>
+                <footer class="project-footer">
+                    <h5 class="project-name">${title}</h5>
+                    <div class="icons">
+                        <a class="project-link" href="${gitLink}" target="_blank"><i class="fa-brands fa-github"></i></a>
+                        ${
+                            (url ==='#') ?
+                            `<a class="project-link url disabled" href="${url}"><i class="fa-solid fa-link"></i></a>`:
+                            `<a class="project-link url" href="${url}" target="_blank"><i class="fa-solid fa-link"></i></a>`
+                        }
+                        
+                    </div>
+                </footer>
+            </article>`;
 
-        </footer>
-    </article>`
     }).join('');
 
-    projectDOM.innerHTML = `<div class="projects-container">
-        ${projectList}
-        </div>`
+    return projectList;
 }
 
-displayProducts(apiProjects);
+const start = ()=>{
+    let projectList = displayProjects(apiProjects);
+    jxaDOM.innerHTML = `
+        <div class="projects-container">
+            ${projectList}
+        </div>`;
+}
+
+start();
