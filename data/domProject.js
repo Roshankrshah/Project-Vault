83684c40deadc843a3997d5cbe8a54d45c3c13d0
Roshan <shahroshan0720@gmail.com>
2023-09-19 16:48:19 +0530

    dom projects added

--- data/domProject.js
+++ data/domProject.js
@@ -1,45 +1,206 @@
 const domProjects =[
     {
         title: "Cocktails API",
-        imgPath: '/images/dom/.jpg',
+        imgPath: '/images/dom/cocktail.jpg',
         gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Cocktails',
         url: 'https://65094fd2e9886222fbe70d2e--cheery-semolina-f731ae.netlify.app/',
-        color: ''
+        color: 'pink'
     },
     {
-        title: "",
-        imgPath: '/images/dom/.jpg',
-        gitLink: '',
-        url: '',
-        color: ''
+        title: "Color Flipper",
+        imgPath: '/images/dom/colorflipper.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/color%20flipper',
+        url: 'https://6509525aa20b4e2ec98ee9f7--beamish-sunburst-349455.netlify.app/hex',
+        color: '#49A6E9'
     },
     {
-        title: "",
-        imgPath: '/images/dom/.jpg',
-        gitLink: '',
-        url: '',
-        color: ''
+        title: "Comfy Store",
+        imgPath: '/images/dom/comfy.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Comfy-store',
+        url: 'https://650952fbef6fec28eeb2c414--lighthearted-scone-2a1f96.netlify.app/',
+        color: '#BA5D2C'
     },
     {
-        title: "",
-        imgPath: '/images/dom/.jpg',
-        gitLink: '',
-        url: '',
-        color: ''
+        title: "Count Down",
+        imgPath: '/images/dom/countdown.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/countdown',
+        url: 'https://65095385ef6fec28eeb368a1--mellow-stardust-4f3354.netlify.app/',
+        color: '#1A6AA2'
     },
     {
-        title: "",
-        imgPath: '/images/dom/.jpg',
-        gitLink: '',
-        url: '',
-        color: ''
+        title: "Counter",
+        imgPath: '/images/dom/counter.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/counter',
+        url: 'https://65095410fbb37b2d45cf0c8b--bucolic-jalebi-e941fe.netlify.app/',
+        color: 'green'
     },
     {
-        title: "",
-        imgPath: '/images/dom/.jpg',
-        gitLink: '',
-        url: '',
-        color: ''
+        title: "Counter-2",
+        imgPath: '/images/dom/counter2.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Counter-2',
+        url: 'https://6509548508aa412fa5900a4d--ornate-wisp-71f534.netlify.app/',
+        color: '#102A42'
+    },
+    {
+        title: "Dad Jokes",
+        imgPath: '/images/dom/dadjoke.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Dad%20jokes',
+        url: 'https://650954ff0c4f2c32d904bbfd--fantastic-babka-9c8a51.netlify.app/',
+        color: '#BA5D2C'
+    },
+    {
+        title: "Dark Mode",
+        imgPath: '/images/dom/darkmode.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Dark%20Mode',
+        url: 'https://650956256403783625a39b5b--astonishing-llama-f51f0d.netlify.app/',
+        color: '#A7E9FF'
+    },
+    {
+        title: "ES6 Slider",
+        imgPath: '/images/dom/es6.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/ES6%20Slider',
+        url: 'https://6509569def6fec2bfdb2c5d0--venerable-conkies-c93fce.netlify.app/',
+        color: '#49A6E9'
+    },
+    {
+        title: "Filters",
+        imgPath: '/images/dom/filters.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Filters',
+        url: 'https://6509572508aa413192900a6c--fascinating-starlight-656a86.netlify.app/',
+        color: 'chocolate'
+    },
+    {
+        title: "Gallery",
+        imgPath: '/images/dom/gallery.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Gallery',
+        url: 'https://65095782a043593639680e84--clinquant-babka-2cbd22.netlify.app/',
+        color: 'coral'
+    },
+    {
+        title: "Grocery Bud",
+        imgPath: '/images/dom/grocery.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/grocery-bud',
+        url: 'https://650957cf25bea334229fd34a--beautiful-halva-4e7e49.netlify.app/',
+        color: '#6BE675'
+    },
+    {
+        title: "Lorem Ipsum Generator",
+        imgPath: '/images/dom/lorem.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Lorem%20Ipsum',
+        url: 'https://65095873fbb37b312ccf146d--prismatic-vacherin-2a2865.netlify.app/',
+        color: 'cornflowerblue'
+    },
+    {
+        title: "Menu",
+        imgPath: '/images/dom/menu.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/menu',
+        url: 'https://65096b875ed2ae3e7728cce7--whimsical-sherbet-59940f.netlify.app/',
+        color: '#C59D5F'
+    },
+    {
+        title: "Modal",
+        imgPath: '/images/dom/modal.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/modal',
+        url: 'https://6509595da20b4e34618ee8f5--neon-pasca-1eb484.netlify.app/',
+        color: 'darkred'
+    },
+    {
+        title: "Navbar",
+        imgPath: '/images/dom/navbar.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/navbar',
+        url: 'https://65095a411e691306fe2dda06--unique-crostata-ce5ed6.netlify.app/',
+        color: 'cadetblue'
+    },
+    {
+        title: "Numbers",
+        imgPath: '/images/dom/numbers.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Numbers',
+        url: 'https://65095a9d5d971a2fabe26698--brilliant-manatee-7e7baf.netlify.app/',
+        color: '#a855f7'
+    },
+    {
+        title: "Pagination",
+        imgPath: '/images/dom/pagination.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Pagination',
+        url: 'https://65095b19ef6fec2f2ab2c716--calm-taiyaki-b8d82b.netlify.app/',
+        color: 'burlywood'
+    },
+    {
+        title: "Products",
+        imgPath: '/images/dom/products.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Products',
+        url: 'https://65095baa5ed2ae34f628cda9--aesthetic-pastelito-9e87ec.netlify.app/',
+        color: '#645CFF'
+    },
+    {
+        title: "Questions",
+        imgPath: '/images/dom/question.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/questions',
+        url: 'https://65095c2189e3b8352b3d2eae--glittering-cobbler-bfc162.netlify.app/',
+        color: '#C59D5F'
+    },
+    {
+        title: "Random Users",
+        imgPath: '/images/dom/random.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Random%20users',
+        url: 'https://65095c9825bea337119fd5d4--super-quokka-fd0d96.netlify.app/',
+        color: 'crimson'
+    },
+    {
+        title: "Reviews",
+        imgPath: '/images/dom/reviews.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Reviews',
+        url: 'https://65095cfa91bdff3d14cc57d2--animated-sundae-18f983.netlify.app/',
+        color: '#2ba8f7'
+    },
+    {
+        title: "Scroll",
+        imgPath: '/images/dom/scroll.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Scroll',
+        url: 'https://65095d9c89e3b835cb3d2b5e--melodious-ganache-3c042a.netlify.app/',
+        color: '#40CFD4'
+    },
+    {
+        title: "Sidebar",
+        imgPath: '/images/dom/sidebar.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/sidebar',
+        url: 'https://65095e085ed2ae365028d12e--sunny-tanuki-809c7f.netlify.app/',
+        color: 'aqua'
+    },
+    {
+        title: "Slider",
+        imgPath: '/images/dom/slider.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Slider',
+        url: 'https://65095e7d5d971a3203e2693e--glittering-fudge-2d273b.netlify.app/',
+        color: 'orange'
+    },
+    {
+        title: "Stripe",
+        imgPath: '/images/dom/stripe.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/stripe-submenus',
+        url: 'https://65095ee790e9f33664c203ac--voluble-lamington-6e6984.netlify.app/',
+        color: '#f9c0ff'
+    },
+    {
+        title: "Tabs",
+        imgPath: '/images/dom/tabs.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Tabs',
+        url: 'https://65095f4608aa41378a900b0b--rainbow-flan-89ae96.netlify.app/',
+        color: 'red'
+    },
+    {
+        title: "Video Project",
+        imgPath: '/images/dom/video.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Video%20project',
+        url: 'https://65095f8b6403783c58a39b95--dreamy-paletas-952c18.netlify.app/',
+        color: 'yellow'
+    },
+    {
+        title: "Wikipedia",
+        imgPath: '/images/dom/wikipedia.jpg',
+        gitLink: 'https://github.com/Roshankrshah/JavaScript-DOM/tree/main/Wikipedia',
+        url: 'https://65096b006403784320a39ba5--fastidious-naiad-9ba631.netlify.app/',
+        color: '#645CFF'
     },
 ];
 
