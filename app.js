6f5d2373b1753f90a6b8d0da9e32cf2d58fa1884
Roshan <shahroshan0720@gmail.com>
2023-10-31 23:54:31 +0530

    load more button setup

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const jxaContainer = document.querySelector('.projects-center.jxa');
 const fullStackContainer = document.querySelector('.projects-center.fullstack');
 const backendContainer = document.querySelector('.projects-center.backend');
 const jsdContainer = document.querySelector('.projects-center.jsd');
+const fsdBtn = document.getElementById('fsd');
+const beBtn = document.getElementById('be');
+const jsxBtn = document.getElementById('jsx');
+const jsdBtn = document.getElementById('jsd');
 
 const displayProjects = (data) => {
+    console.log(data);
     const projectList = data.map((project) => {
         const title = project.title;
         const imgPath = project.imgPath;
@@ -39,26 +44,97 @@ const displayProjects = (data) => {
     return projectList;
 }
 
+let jxa_itr = 6, fs_itr = 6, b_itr = 6, jsd_itr = 6;
+
+
+fsdBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if ((fs_itr + 6) < fullStackProjects.length)
+        fs_itr += 6;
+    else {
+        fs_itr = fullStackProjects.length;
+        fsdBtn.remove();
+    }
+
+    let projectList = displayProjects(fullStackProjects.slice(0, fs_itr));
+    fullStackContainer.innerHTML = `
+        <div class="projects-container">
+            ${projectList}
+        </div>`;
+});
+
+beBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if ((b_itr + 6) < backendProjects.length)
+        b_itr += 6;
+    else {
+        b_itr = backendProjects.length;
+        beBtn.remove();
+    }
+
+    let projectList = displayProjects(backendProjects.slice(0, b_itr));
+    backendContainer.innerHTML = `
+        <div class="projects-container">
+            ${projectList}
+        </div>`;
+});
+
+jsxBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if ((jxa_itr + 6) < apiProjects.length)
+        jxa_itr += 6;
+    else {
+        jxa_itr = apiProjects.length;
+        jsxBtn.remove();
+    }
+
+    let projectList = displayProjects(apiProjects.slice(0, jxa_itr));
+    jxaContainer.innerHTML = `
+        <div class="projects-container">
+            ${projectList}
+        </div>`;
+});
+
+jsdBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if ((jsd_itr + 6) < domProjects.length)
+        jsd_itr += 6;
+    else {
+        jsd_itr = domProjects.length;
+        jsdBtn.remove();
+    }
+
+    let projectList = displayProjects(domProjects.slice(0, jsd_itr));
+    jsdContainer.innerHTML = `
+        <div class="projects-container">
+            ${projectList}
+        </div>`;
+});
+
 const start = () => {
-    let projectList = displayProjects(apiProjects);
+    let projectList = displayProjects(apiProjects.slice(0, jxa_itr));
     jxaContainer.innerHTML = `
         <div class="projects-container">
             ${projectList}
         </div>`;
 
-    projectList = displayProjects(fullStackProjects);
+    projectList = displayProjects(fullStackProjects.slice(0, fs_itr));
     fullStackContainer.innerHTML = `
         <div class="projects-container">
             ${projectList}
         </div>`;
 
-    projectList = displayProjects(backendProjects);
+    projectList = displayProjects(backendProjects.slice(0, b_itr));
     backendContainer.innerHTML = `
         <div class="projects-container">
             ${projectList}
         </div>`;
 
-    projectList = displayProjects(domProjects);
+    projectList = displayProjects(domProjects.slice(0, jsd_itr));
     jsdContainer.innerHTML = `
         <div class="projects-container">
             ${projectList}
